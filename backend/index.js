dcf57d31e8f49c7bcb0f4ccd2c2be4c1888b3872
rohitBaraik001 <136783344+rohitBaraik001@users.noreply.github.com>
2024-02-11 23:28:11 +0530

    kjbov

--- backend/index.js
+++ backend/index.js
@@ -51,14 +51,15 @@ const io = require('socket.io')(server, {
         methods:["GET", "POST"]
     },
 
-    // handlePreflightRequest: (req,res)=>{
-    //     const headers = {
-    //         "Access-Control-Allow-Headers":"Content-Type, Authorization",
-    //         "Access-Control-Allow-Origin":req.headers.origin,
-    //         "Access-Control-Allow-Credentials": true
-    //     }
-    //     res.writeHead(200,headers)
-    //     res.end()
+    handlePreflightRequest: (req,res)=>{
+        const headers = {
+            "Access-Control-Allow-Headers":"Content-Type, Authorization",
+            "Access-Control-Allow-Origin":req.headers.origin,
+            "Access-Control-Allow-Credentials": true
+        }
+        res.writeHead(200,headers)
+        res.end()
+    }
 });
 
 console.log("herer");
